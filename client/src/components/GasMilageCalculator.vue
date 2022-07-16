7b6a9589aaeacd6e5e97f16bd968bd2e6b49bde2
Chavez Harris <codedbychavez@gmail>
2022-07-16 15:55:36 -0300

    Added draft skeleton

---
+++ client/src/components/GasMilageCalculator.vue
@@ -0,0 +1,26 @@
+<script>
+// Imports
+import GasMilageForm from './GasMilageForm.vue'
+
+export default {
+  name: 'gas-milage-calculator',
+  setup() {
+    
+  },
+  components: {
+    GasMilageForm
+  },
+}
+</script>
+
+<template>
+
+<div class="p-4 w-full bg-white rounded-lg sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+  <h5 class="text-center mb-2 text-3xl font-bold text-gray-900 dark:text-white">Calculate Your Travel Milage</h5>
+  <p class="text-center mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Stay up to date and move it forward with eco fuel. More milage for less dollars.</p>
+  <div class="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+    <GasMilageForm />
+  </div>
+</div>
+
+</template>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,41 @@
 <script>
 // Imports
 import AppBanner from '@/components/AppBanner.vue'
+import GasMilageCalculator from '@/components/GasMilageCalculator'
+import SubscriptionCTA from '@/components/SubscriptionCTA.vue'
 
 export default {
   name: 'home-page',
   setup() {},
   components: {
     AppBanner,
+    GasMilageCalculator,
+    SubscriptionCTA,
+  },
+  data() {
+    return {
+      isMilageCalculatorAvailable: false
+    }
+  },
+  mounted() {
+    fetch("https://api.npms.io/v2/invalid-url")
+      .then(async response => {
+        const data = await response.json();
+
+        // check for error response
+        if (!response.ok) {
+          // get error message from body or default to response statusText
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
+        }
+
+        this.isMilageCalculatorAvailable = true;
+      })
+      .catch(error => {
+        this.errorMessage = error;
+        console.error("There was an error!", error);
+        this.isMilageCalculatorAvailable = false;
+      });
   }
 }
 
@@ -15,6 +44,15 @@ export default {
 <template>
  <div>
     <AppBanner />
+
+    <div class="flex justify-center items-center">
+      <div class="container" v-if="isMilageCalculatorAvailable">
+      <GasMilageCalculator />
+      </div>
+      <div v-else class="container">
+        <SubscriptionCTA />
+      </div>
+    </div>
  </div>
 
 </template>
