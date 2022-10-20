9348e23696e9fc9ea06c229aaeb92b9b64dcc32c
codedbychavez <codedbychavez@gmail.com>
2022-10-20 09:36:36 -0300

    Fixed linting errors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,29 +3,32 @@ import { AppService } from './app.service';
 import { IConfigCatClient } from 'configcat-common';
 
 // Import the ConfigCat JavaScript SSR client SDK we've installed
-import * as configcat from "configcat-js-ssr";
+import * as configcat from 'configcat-js-ssr';
 
 @Controller('travel')
 export class AppController {
   constructor(private readonly appService: AppService) {
     // Initialize the ConfigCat client with your SDK key
-    this.configCatClient = configcat.createClient("-VbaCFs0RUGAghJLdTal-g/ZRvIWKTu0k-ZXxhOiebpBQ");
+    this.configCatClient = configcat.createClient('YOUR_CONFIGCAT_SDK_KEY');
   }
 
   @Get('mileage')
-  async getMileageFeature(): Promise<Boolean> {
+  async getMileageFeature(): Promise<boolean> {
     // Create a variable to store the state of the feature flag from ConfigCat.
     // This variable will be automatically updated every 60 seconds by default.
-    const canShowMileageFeature = await this.configCatClient.getValueAsync("canshowmileagefeature", false)
+    const canShowMileageFeature = await this.configCatClient.getValueAsync(
+      'canshowmileagefeature',
+      false,
+    );
 
     if (canShowMileageFeature) {
       // When true is returned to the frontend, the Gas Mileage feature is rendered
-      return true
+      return true;
     } else {
       // When false is returned to the frontend, the Gas Mileage feature is not rendered. Instead, the subscription form will be rendered.
-      return false
+      return false;
     }
   }
 
-  public configCatClient: IConfigCatClient
+  public configCatClient: IConfigCatClient;
 }
