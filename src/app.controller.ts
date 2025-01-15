448eb2376115d95e0a0952615280fa4d7ad3e94a
Chavez Harris <codedbychavez@gmail.com>
2025-01-15 19:15:19 -0400

    Updated app.controller and app.service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response): void {
+    const viewName = this.appService.getViewName();
+    res.render(viewName, { message: this.appService.getHello() });
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  getViewName(): string {
+    // Implement the logic to return the name of the view file
+    return 'index';
+  }
   getHello(): string {
     return 'Hello World!';
   }
